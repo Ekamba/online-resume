436090664f2068474df66c94d36ef45ef4c63560
Ekamba <axelgassay@yahoo.dk>
2022-07-27 11:19:49 +0200

    refactoring

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,77 +1,50 @@
 export const projectsData = [
   {
     id: 1,
-    image: "/assets/disney+.png",
-    title: "Disney+ application",
+    image: '/assets/roomkast.png',
+    title: 'Roomkast application',
     description:
-      "This is a React application that allows users to search for movies and shows from the Disney+ website. The application uses the TMDB API to fetch data from the website and display it on the page. The application also allows users to add movies to their favorites list and view their favorites list.",
-    github: "https://github.com/Ekamba/Disney_plus",
-    liveDemo: "https://disneyplus-fac25.web.app/home",
+      'This application allows the user to find an apartment or room in featured areas or close to your interest points.',
+    github: 'https://github.com/Ekamba/roomkast',
+    liveDemo: 'https://roomkast.netlify.app/',
   },
   {
     id: 2,
-    image: "/assets/memories.png",
-    title: "Memories application",
+    image: '/assets/soccer.png',
+    title: 'Soccer league Application',
     description:
-      "This a Full Stack MERN Application. Using React, Node.js, Express & MongoDB. The App is called 'Memories' and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-    github: "https://github.com/Ekamba/Souvenirs",
-    liveDemo: "https://souvenir-memories.netlify.app/posts",
+      'A football leagues application that shoes how to work with apis and so much more using React and his features.',
+    github: 'https://github.com/Ekamba/soccer-league',
+    liveDemo: 'https://soccer-leagues.netlify.app/',
   },
   {
     id: 3,
-    image: "/assets/Model3.png",
-    title: "Tesla application",
+    image: '/assets/final_project11.png',
+    title: 'Dashboard Application',
     description:
-      "A Full Stack App using React, Redux-toolkit, Styled-components, and firebase. The App is called 'Tesla clone' is inspired by the Tesla application, which sells electric cars, battery energy storage from home to grid-scale, solar panels...",
-    github: "https://github.com/Ekamba/Tesla-clone",
-    liveDemo: "https://tesla-electric-car.netlify.app/",
-  },
-  {
-    id: 4,
-    image: "/assets/roomkast.png",
-    title: "Roomkast application",
-    description:
-      "This application allows the user to find an apartment or room in featured areas or close to your interest points.",
-    github: "https://github.com/Ekamba/roomkast",
-    liveDemo: "https://roomkast.netlify.app/",
-  },
-  {
-    id: 5,
-    image: "/assets/get-social.png",
-    title: "Get-social Application",
-    description:
-      "A social media platform for connecting with friends and family.",
-    github: "https://github.com/Ekamba/Get_Social",
-    liveDemo: "https://get-social.herokuapp.com",
-  },
-  {
-    id: 6,
-    image: "/assets/soccer.png",
-    title: "Soccer league Application",
-    description:
-      "A football leagues application that shoes how to work with apis and so much more using React and his features.",
-    github: "https://github.com/Ekamba/soccer-league",
-    liveDemo: "https://soccer-leagues.netlify.app/",
+      'Final project: A dashboard for tracking relevant sensors, developed as a group project at Hack Your Future - Copenhagen, in collaboration with Seasony which is a startup specializing in vertical farming with robotics.',
+    github: 'https://github.com/Ekamba/FP-class11',
+    liveDemo: ' http://fp-class11.herokuapp.com/',
   },
 ];
 
 export const experiencesData = [
   {
-    avatar: "/assets/key_icon.png",
-    name: "Roomkast Aps",
+    avatar: '/assets/key_icon.png',
+    name: 'Roomkast Aps',
     review:
-      "Using ReactJS, JavaScript and CSS to bring concepts to life. Optimizing the user experience. Developing and maintaining the user interface. Implementing design on mobile websites. Creating tools that improve site interaction regardless of the browser. Managing software workflow. Following SEO best practices. Fixing bugs and testing for usability.",
+      'Using ReactJS, JavaScript and CSS to bring concepts to life. Optimizing the user experience. Developing and maintaining the user interface. Implementing design on mobile websites. Creating tools that improve site interaction regardless of the browser. Managing software workflow. Following SEO best practices. Fixing bugs and testing for usability.',
   },
   {
-    avatar: "/assets/bluebenu.png",
-    name: "Bluebenu",
+    avatar: '/assets/bluebenu.png',
+    name: 'Bluebenu',
     review:
-      "Develop and design the software of the company using ReactJS, NodeJS, Express, MongoDB, and Material UI. Maintain and update the software to the latest technology. Identify problems uncovered by customer feedback and testing, and correcting or referring problems to appropriate personnel for correction.",
+      'Develop and design the software of the company using ReactJS, NodeJS, Express, MongoDB, and Material UI. Maintain and update the software to the latest technology. Identify problems uncovered by customer feedback and testing, and correcting or referring problems to appropriate personnel for correction.',
   },
   {
-    avatar: "/assets/ating.jpg",
-    name: "Ating Aps",
+    avatar: '/assets/ating.jpg',
+    name: 'Ating Aps',
     review:
-      "Build the software using ReactJS and React Native. Determining user needs by analyzing technical requirements. Directing and performing Website update.",
+      'Build the software using ReactJS and React Native. Determining user needs by analyzing technical requirements. Directing and performing Website update.',
   },
 ];
